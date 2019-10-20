669f69007e97541efbcbd74d46f5172999bfe258
Igifoshifo <matthew.abernathy@hotmail.com>
2019-10-20 18:46:52 -0400

    Added error message. Added shell of site

--- 499homework/src/App.js
+++ 499homework/src/App.js
@@ -1,37 +1,75 @@
 import React, { Component } from 'react';
-//import MyNavbar from './MyNav/MyNavbar.js';
 import { Navbar, Nav } from 'react-bootstrap';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 /*
   Variables
 */
-const DEFAULT_QUERY = 'redux';
-const DEFAULT_PAGE = 0;
-const DEFAULT_HPP = '100';
-
-const PATH_BASE = 'https://hn.algolia.com/api/v1'; 
-const PATH_SEARCH = '/search'; 
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
+const PATH_BASE = 'http://gtest.dev.wwbtc.com'; 
+const PATH_JSON = '/json'; 
+const PARAM_REC = '/rec';
 
 /*
   Components
-  TODO: Organize to folder space
 */
+const Recipe = (recipesList) =>
+{
+  return (
+    <div className="recipe">
+      { recipesList.map(item =>
+        <div key={item.title}>
+          <span>
+            {item.title}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const Error = () =>
+{
+  return (
+    <div className="error">
+      Something went wrong..
+    </div>
+  );
+}
 
 /*
   Main/Initial
 */
-class App extends Component {
-  // constructor of properties
-  constructor(props) {
+class App extends Component 
+{
+  constructor(props) 
+  {
     super(props);
 
     this.state = {
-      isLoading: false,
+      recipes: null,
     }
 
+    this.fetchRecipes = this.fetchRecipes.bind(this);
+    this.setRecipes = this.fetchRecipes.bind(this);
+  }
+
+  setRecipes(recipes)
+  {
+    this.setState({ recipes });
+  }
+
+  fetchRecipes(PARAM_REC)
+  {
+    fetch(`${PATH_BASE}${PATH_JSON}${PARAM_REC}`)
+    .then(response => response.json())
+    .then(recipes => this.setRecipes(recipes));
+  }
+
+  componentDidMount() 
+  { 
+    //const { searchTerm } = this.state; 
+    this.fetchRecipes('/rec'); 
   }
 
   /*
@@ -41,22 +79,46 @@ class App extends Component {
   /*
     Rendered Elements
   */
-  render() {
-    const {
-      isLoading
-    } = this.state;
+  render() 
+  {
+    const { recipes } = this.state;
+
+    if (!recipes) 
+    {
+      return (
+        <div>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Umami Replica</Navbar.Brand>
+            <Navbar.Toggle />
+            <Navbar.Collapse>
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#recipes">Recipes</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Error />
+        </div>
+      );
+    }
 
     return (
       <div className="App">
         <Navbar bg="light" expand="lg">
           <Navbar.Brand href="#home">Umami Replica</Navbar.Brand>
-          <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Toggle/>
+          <Navbar.Collapse>
             <Nav className="mr-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#recipes">Recipes</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
+
+        <Recipe
+          recipesList = {recipes.hits}
+        />
       </div>
     );
   }
