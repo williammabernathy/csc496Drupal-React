56d85ade0dc8abc91a5a2ebfdae4c1c83bd7c77d
Igifoshifo <matthew.abernathy@hotmail.com>
2019-11-10 14:41:40 -0500

    Basic setup of enzyme

--- 499homework/src/App.spec.js
+++ 499homework/src/App.spec.js
@@ -1,3 +1,16 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from './App';
+
+describe('App', () => 
+{
+    test('snapshot renders', () => {
+        const component = renderer.create(<App />);
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
 describe('My Test Suite', () => 
 {
     it('My Test Case', () => 
