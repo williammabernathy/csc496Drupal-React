a3da2815bf016702f3522bbf844e6e0a7fe73b99
Igifoshifo <matthew.abernathy@hotmail.com>
2019-11-12 13:11:55 -0500

    Added README. Began basic formatting

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const fetch = require("node-fetch");
+
+//api url variables
+export const PATH_BASE = 'http://gtest.dev.wwbtc.com'; 
+export const PATH_JSON = '/json'; 
+export const PARAM_TYPE = '/rec';
+
+//variables for changing url
+export const recURL = "#recipe";
+export const homeURL = "#home";
